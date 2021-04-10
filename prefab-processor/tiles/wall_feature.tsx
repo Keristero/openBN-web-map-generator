b45164ab23220ac6eca4c9aaf3a81a7914793052
Keristero Kerivinix <Keristero@gmail.com>
2021-04-10 20:32:51 +1200

    update prefab tiles and maps to remove flipping

--- prefab-processor/tiles/wall_feature.tsx
+++ prefab-processor/tiles/wall_feature.tsx
@@ -1,5 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="2021.02.15" name="wall_feature" tilewidth="64" tileheight="64" tilecount="1" columns="1" objectalignment="bottom">
- <transformations hflip="1" vflip="0" rotate="0" preferuntransformed="0"/>
- <image source="wall_feature.png" width="64" height="64"/>
+<tileset version="1.5" tiledversion="1.5.0" name="wall_feature" tilewidth="64" tileheight="64" tilecount="2" columns="2" objectalignment="bottom">
+ <image source="wall_feature.png" width="128" height="64"/>
 </tileset>
