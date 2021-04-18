f1ab65e5d8eba14cd5298036e052f923214c491c
Keristero Kerivinix <Keristero@gmail.com>
2021-04-18 18:04:53 +1200

    fix back stair direction to match fixed server

--- onb-server/assets/shared/tiles/back-stairs.tsx
+++ onb-server/assets/shared/tiles/back-stairs.tsx
@@ -2,37 +2,37 @@
 <tileset version="1.5" tiledversion="1.5.0" name="back-stairs" tilewidth="92" tileheight="40" tilecount="6" columns="2">
  <tileoffset x="-16" y="8"/>
  <properties>
-  <property name="Direction" value="Down Left"/>
+  <property name="Direction" value="Down Right"/>
  </properties>
  <image source="back-stairs.png" width="184" height="120"/>
  <tile id="0" type="Stairs">
   <properties>
-   <property name="Direction" value="Down Left"/>
+   <property name="Direction" value="Down Right"/>
   </properties>
  </tile>
  <tile id="1" type="Stairs">
   <properties>
-   <property name="Direction" value="Down Right"/>
+   <property name="Direction" value="Down Left"/>
   </properties>
  </tile>
  <tile id="2" type="Stairs">
   <properties>
-   <property name="Direction" value="Down Left"/>
+   <property name="Direction" value="Down Right"/>
   </properties>
  </tile>
  <tile id="3" type="Stairs">
   <properties>
-   <property name="Direction" value="Down Right"/>
+   <property name="Direction" value="Down Left"/>
   </properties>
  </tile>
  <tile id="4">
   <properties>
-   <property name="Direction" value="Down Left"/>
+   <property name="Direction" value="Down Right"/>
   </properties>
  </tile>
  <tile id="5">
   <properties>
-   <property name="Direction" value="Down Right"/>
+   <property name="Direction" value="Down Left"/>
   </properties>
  </tile>
 </tileset>
