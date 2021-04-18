f5d58c35fdd6326e5f49a5c58cc4a6ea6abb4357
Keristero Kerivinix <Keristero@gmail.com>
2021-04-18 20:51:05 +1200

    update to use new scriptable server promises

--- onb-server/scripts/hyperlinks.lua
+++ onb-server/scripts/hyperlinks.lua
@@ -1,6 +1,6 @@
 local json = require('scripts/libs/json')
 
-local promises = {}
+local currently_generating = {}
 
 function handle_object_interaction(player_id, object_id)
     local area_id = Net.get_player_area(player_id)
@@ -10,30 +10,31 @@ function handle_object_interaction(player_id, object_id)
         return
     end
 
-    --TODO insert check here to see if the map is already loaded
+    local web_link = object.custom_properties.link
 
-    if promises[area_id..object_id] then
+    --TODO insert check here to see if the map is already loaded
+    if currently_generating[web_link] then
         print("[hyperlinks] already generating this area....")
         return
     end
+    
 
     --Geneate a map
     local generate_map_promise = generate_linked_map(player_id, object_id, object)
-    promises[area_id..object_id] = generate_map_promise
+    generate_map_promise.and_then(function (area_info)
 
-    generate_map_promise.after = function (area_info)
         --Read new generated map file
         local read_file_promise = Async.read_file(area_info.area_path)
-        promises[area_info.area_path] = read_file_promise
-
-        read_file_promise.after = function (area_data)
+        read_file_promise.and_then(function (area_data)
+            currently_generating[web_link] = nil
             --Add area to server, transfer player there
             Net.update_area(area_info.area_id, area_data)
             print("[hyperlinks] added area "..area_info.area_id)
             Net.transfer_player(player_id, area_info.area_id)
-        end
+            
+        end)
 
-    end
+    end)
 
 end
 
@@ -67,15 +68,4 @@ function generate_linked_map(player_id, object_id, object)
         return data
     end)
     return Async.promisify(co)
-end
-
-function tick()
-    for promise_id, promise in pairs(promises) do
-        if promise.is_ready() then
-            if promise.after ~= nil then
-                promise.after(promise.get_value())
-            end
-            promises[promise_id] = nil
-        end
-    end
 end
