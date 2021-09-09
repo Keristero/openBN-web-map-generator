0c2eab30ad824cf4db3079b6b0b544afcf95e674
Keristero Kerivinix <Keristero@gmail.com>
2021-09-10 10:29:53 +1200

    wip prefab generation

--- new-map-generator/NetPrefabGenerator.js
+++ new-map-generator/NetPrefabGenerator.js
@@ -41,6 +41,8 @@ function GenerateForRequirements({ ground_features, wall_features }) {
 }
 
 function write_parts_to_prefab(prefab, placements) {
+    const wall_padding = 1 //a tile on each size to allow for empty space where paths are expensive for pathfinding (walls)
+    const wall_id = 1 //tile id of walls
     //Find dimensions of prefab and generate a 3d array with the same dimensions
     let part_max_x = -Infinity
     let part_max_y = -Infinity
@@ -52,53 +54,57 @@ function write_parts_to_prefab(prefab, placements) {
         let part_end_x = placement.part_pos.x + placement.part.width;
         let part_end_y = placement.part_pos.y + placement.part.length;
         let part_end_z = placement.part_pos.z + placement.part.height;
-        if (part_end_x > part_max_x) {
+        if (part_end_x >= part_max_x) {
             part_max_x = part_end_x;
         }
-        if (part_end_y > part_max_y) {
+        if (part_end_y >= part_max_y) {
             part_max_y = part_end_y;
         }
-        if (part_end_z > part_max_z) {
+        if (part_end_z >= part_max_z) {
             part_max_z = part_end_z;
         }
-        if (placement.part_pos.x < part_start_x) {
+        if (placement.part_pos.x <= part_start_x) {
             part_start_x = placement.part_pos.x
         }
-        if (placement.part_pos.y < part_start_y) {
+        if (placement.part_pos.y <= part_start_y) {
             part_start_y = placement.part_pos.y
         }
-        if (placement.part_pos.z < part_start_z) {
+        if (placement.part_pos.z <= part_start_z) {
             part_start_z = placement.part_pos.z
         }
     }
-    let width = Math.abs(part_max_x-part_start_x)
-    let length = Math.abs(part_max_y-part_start_y)
+    let width = Math.abs(part_max_x-part_start_x)+(wall_padding*2)
+    let length = Math.abs(part_max_y-part_start_y)+(wall_padding*2)
     let height = Math.abs(part_max_z-part_start_z)+1
     console.log('prefab dimensions',width,length,height)
-    prefab.matrix = generate3dMatrix(width,length,height,0)
+    prefab.matrix = generate3dMatrix(width,length,height,wall_id)
 
     //loop over each part and copy it's array contents to the 3d array
     for (let placement of placements) {
-        console.log(placement)
         let part_matrix_iterator = iterateOver3dMatrix(placement.part.matrix)
         for(let grid_pos of part_matrix_iterator){
-            let x = -part_start_x + placement.part_pos.x + grid_pos.x
-            let y = -part_start_y + placement.part_pos.y + grid_pos.y
+            let x = -part_start_x + placement.part_pos.x + grid_pos.x + wall_padding
+            let y = -part_start_y + placement.part_pos.y + grid_pos.y + wall_padding
             let z = -part_start_z + placement.part_pos.z + grid_pos.z
             prefab.matrix[z][y][x] = grid_pos.tileID
         }
-        console.log(prefab.matrix)
     }
 
     //add each feature from each part into the prefab
     let feature_collections = ['ground_features','male_connectors','female_connectors', 'wall_features']
+    let directions = ['Up Left','Up Right','Down Left','Down Right']
     for (let feature_collection_name of feature_collections) {
         for (let placement of placements) {
             let part = placement.part
             for (let feature of part[feature_collection_name]) {
-                let x = -placement.part_pos.x + feature.x
-                let y = -placement.part_pos.y + feature.y
-                let z = -placement.part_pos.z + feature.z
+                let x = placement.part_pos.x + feature.x + wall_padding
+                let y = placement.part_pos.y + feature.y + wall_padding
+                let z = placement.part_pos.z + feature.z
+                if(feature.properties){
+                    if(!feature.properties.Direction){
+                        feature.properties.Direction = directions[Math.floor(Math.random()*directions.length)]
+                    }
+                }
                 prefab.AddFeature(feature_collection_name, x, y, z, feature.properties)
             }
         }
@@ -131,29 +137,53 @@ function place_part(part, part_pos, placed_features, placements) {
 
 function find_placement(partA, placements, placed_features) {
     console.log(`finding placement for`, partA.name)
-    let partA_pos = {
-        x: 0,
-        y: 0,
-        z: 0,
-    }
 
     let female_connectors = placed_features.arr_female_connectors
 
     //If no other parts are placed yet, just place this one
     if (placements.length == 0) {
-        return partA_pos
+        return {
+            x: 0,
+            y: 0,
+            z: 0,
+        }
     }
 
     //first naive approach, just connect first male+female connector pair that works without collisions
+    /*
     for (let male_connector of partA.male_connectors) {
         for (let female_connector of female_connectors) {
             partA_pos.x = female_connector.x - male_connector.x
             partA_pos.y = female_connector.y - male_connector.y
             partA_pos.z = female_connector.z - male_connector.z
             if (!does_part_at_position_overlap_placed_part(partA, partA_pos, placements)) {
+                console.log('lmoa')
                 return partA_pos
             }
         }
+    }
+    */
+
+    //more random approach, add all valid placements to list and pick one randomly
+    let valid_placements = []
+    for (let male_connector of partA.male_connectors) {
+        for (let female_connector of female_connectors) {
+            let partA_pos = {
+                x: 0,
+                y: 0,
+                z: 0,
+            }
+            partA_pos.x = female_connector.x - male_connector.x
+            partA_pos.y = female_connector.y - male_connector.y
+            partA_pos.z = female_connector.z - male_connector.z
+            if (!does_part_at_position_overlap_placed_part(partA, partA_pos, placements)) {
+                valid_placements.push(partA_pos)
+            }
+        }
+    }
+    if(valid_placements.length > 0){
+        console.log('valid placements',valid_placements)
+        return valid_placements[Math.floor(Math.random() * valid_placements.length)]
     }
     console.warn(`no placement found for part`)
     return null
@@ -166,7 +196,7 @@ function does_part_at_position_overlap_placed_part(partA, partA_pos, placements)
 
         let z_overlap = partA_pos.z <= partB_pos.z + partB.height && partA_pos.z + partA.height > partB_pos.z
         let y_overlap = partA_pos.y <= partB_pos.y + partB.length && partA_pos.y + partA.length > partB_pos.y
-        let x_overlap = partA_pos.x <= partB_pos.z + partB.width && partA_pos.x + partA.width > partB_pos.x
+        let x_overlap = partA_pos.x <= partB_pos.x + partB.width && partA_pos.x + partA.width > partB_pos.x
 
         if (z_overlap && y_overlap && x_overlap) {
             return true
