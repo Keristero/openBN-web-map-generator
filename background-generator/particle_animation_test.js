8169825a8973589365de79f7895aa635eeffd365
Keristero <Keristero@gmail.com>
2021-12-08 14:05:26 +1300

    now animating snow

--- background-generator/particle_animation_test.js
+++ background-generator/particle_animation_test.js
@@ -1,12 +1,14 @@
 const fs = require("fs");
 const path = require('path');
 const { createCanvas, loadImage } = require('canvas')
-const { downloadFavicon } = require('./downloadFavicon.js')
-const { RNG } = require('../helpers.js')
 const TweenJs = require('@tweenjs/tween.js')
-const Random = new RNG()
 //For preview
 const GIFEncoder = require('gifencoder');
+
+function RandomInteger(min, max) {
+    var range = max - min + 1
+    return Math.floor(range * Math.random()) + min
+}
 
 async function generate_particle_animation(outputName,outputPath){
     let output_filename = `${outputName}.png`
@@ -25,7 +27,7 @@ async function generateAnimationFile(PNGname,outputPath,width,height,frames){
     for(let i = 0; i < frames; i++){
         let x = i % frames_per_row
         let y = Math.floor(i / frames_per_row)
-        outputString += `frame duration="0.041" x="${x*width}" y="${y*height}" w="${width}" h="${height}" originx="0" originy="0"\n`
+        outputString += `frame duration="0.032" x="${x*width}" y="${y*height}" w="${width}" h="${height}" originx="0" originy="0"\n`
     }
     fs.writeFileSync(outputPath,outputString)
 }
@@ -50,7 +52,7 @@ async function generateAnimationPNG(output_path, preview = true, previewPath = "
         encoder.createReadStream().pipe(fs.createWriteStream(previewPath));
         encoder.start();
         encoder.setRepeat(0);   // 0 for repeat, -1 for no-repeat
-        encoder.setDelay(16);  // frame delay in ms
+        encoder.setDelay(32);  // frame delay in ms
         encoder.setQuality(10); // image quality. 10 is default.
     }
 
@@ -81,48 +83,69 @@ class ParticleAnimation {
     constructor(canvas, width,height) {
         this.width = width
         this.height = height
-        //rain animation
+        //snow animation
         this.particles = {}
+        this.particles_linear = {}
         this.uninterpolated_particle_properties = {}
-        for(let i = 0; i< 30; i ++){
-            let x = Random.Integer(0, this.width)
-            let y = Random.Integer(0, this.height)
-            let scale = Random.Integer(0,2)
-            this.particles[i]= {x:x,y:y,w:1*scale,h:2*scale,r:200,g:200,b:255,a:0.5}
-            this.uninterpolated_particle_properties[i] = {scale:scale}
+        for(let i = 0; i< 50; i ++){
+            let x = RandomInteger(0, this.width)
+            let y = RandomInteger(0, this.height)
+            let scale = 1+(Math.random()*2)
+            this.particles[i]= {x:x,w:1*scale,h:2*scale,r:200,g:200,b:255,a:0.5}
+            this.particles_linear[i]= {y:y}
+            this.uninterpolated_particle_properties[i] = {scale:scale,horz_dist:RandomInteger(-10,10)*scale,originalx:x}
         }
 
         this.tweenTargets = []
-        let animationStages = 1
-        for (let stage = 0; stage < animationStages; stage++) {
-            this.generateTweenStage(this.particles)
+        this.tweenTargetsLinear = []
+        this.animation_stages = 2
+        for (let stage = 0; stage < this.animation_stages; stage++) {
+            if(stage == 0){
+                this.generateTweenStage(this.particles,this.particles_linear,stage)
+            }else{
+                this.generateTweenStage(this.tweenTargets[this.tweenTargets.length-1],this.tweenTargetsLinear[this.tweenTargetsLinear.length-1],stage)
+            }
         }
-        this.frames = 64 *animationStages
+        this.frames = 32 *this.animation_stages
 
-        let easeStrat = TweenJs.Easing.Linear.None
+        let easeStrat = TweenJs.Easing.Sinusoidal.InOut
         console.log('frames', this.frames)
-        console.log('stages', animationStages)
+        console.log('stages', this.animation_stages)
 
         let tweens = []
+        let tweens_linear = []
         let index = 0
-        for (let tweenTarget of this.tweenTargets) {
+        for (let tween_index in this.tweenTargets) {
+            let tweenTarget = this.tweenTargets[tween_index]
+            let tweenTargetLinear = this.tweenTargetsLinear[tween_index]
             let tweenDuration = Math.floor(this.frames / this.tweenTargets.length)
             let newTween = new TweenJs.Tween(this.particles).to(tweenTarget, tweenDuration).easing(easeStrat)
+            let newTweenLinear = new TweenJs.Tween(this.particles_linear).to(tweenTargetLinear, tweenDuration).easing(TweenJs.Easing.Linear.None)
             if (index > 0) {
                 tweens[index - 1].chain(newTween)
+                tweens_linear[index - 1].chain(newTweenLinear)
             } else {
                 newTween.start(0)
+                newTweenLinear.start(0)
             }
             tweens.push(newTween)
+            tweens_linear.push(newTweenLinear)
             index++
         }
-        tweens[0].start()
+        //tweens[0].start()
+        //tweens_linear[0].start()
     }
-    update_particles(particles) {
+    update_particles(particles,particles_linear,stage_index) {
+        console.log(stage_index)
         for (let particle_id in particles) {
             let particle = particles[particle_id]
-            let particle_extra_info = this.uninterpolated_particle_properties[particle_id]
-            particle.y += this.height*particle_extra_info.scale
+            let particle_linear = particles_linear[particle_id]
+            let particle_info = this.uninterpolated_particle_properties[particle_id]
+            particle_linear.y += (this.height/this.animation_stages)*Math.floor(particle_info.scale)
+            particle.x += particle_info.horz_dist
+            if(stage_index == this.animation_stages-1){
+                particle.x = particle_info.originalx
+            }
         }
     }
     animateFrame(ctx, frame) {
@@ -133,28 +156,37 @@ class ParticleAnimation {
         //draw the particles
         for (let particle_id in this.particles) {
             let particle = this.particles[particle_id]
+            let particle_linear = this.particles_linear[particle_id]
             ctx.fillStyle = `rgba(${particle.r},${particle.g},${particle.b},${particle.a})`
             let centerx = particle.x-(particle.w*0.5)
-            let centery = particle.y-(particle.h*0.5)
+            let centery = particle_linear.y-(particle.h*0.5)
             let wrappedx = (centerx%this.width)
             let wrappedy = (centery%this.height)
-            ctx.fillRect(wrappedx,wrappedy,particle.w,particle.h)
+            draw_circle(ctx,wrappedx,wrappedy,particle.w)
             //if the particle will be moving into the top of the frame draw it there too
             //y coord
-            ctx.fillRect(wrappedx,wrappedy-this.height,particle.w,particle.h)
-            ctx.fillRect(wrappedx,wrappedy+this.height,particle.w,particle.h)
+            draw_circle(ctx,wrappedx,wrappedy-this.height,particle.w)
+            draw_circle(ctx,wrappedx,wrappedy+this.height,particle.w)
             //x coord
-            ctx.fillRect(wrappedx-this.width,wrappedy,particle.w,particle.h)
-            ctx.fillRect(wrappedx+this.width,wrappedy,particle.w,particle.h)
+            draw_circle(ctx,wrappedx-this.width,wrappedy,particle.w)
+            draw_circle(ctx,wrappedx+this.width,wrappedy,particle.w)
             //ctx.fillRect(centerx%this.width,centery%this.height,particle.w,particle.h)
             //draw particle
         }
     }
-    generateTweenStage(previousStage) {
+    generateTweenStage(previousStage,previousStageLinear,stage_index) {
         var particles_clone = JSON.parse(JSON.stringify(previousStage))
-        this.update_particles(particles_clone)
+        var particles_clone_linear = JSON.parse(JSON.stringify(previousStageLinear))
+        this.update_particles(particles_clone,particles_clone_linear,stage_index)
         this.tweenTargets.push(particles_clone)
+        this.tweenTargetsLinear.push(particles_clone_linear)
     }
+}
+
+function draw_circle(ctx,x,y,radius){
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.fill()
 }
 
 function saveOutputFrame(canvas, preview, out_ctx, out_sheet_row_length, preview_ctx, encoder, frame_index) {
