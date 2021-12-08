ea7a8de4a09753e3266c33b1fe9ff88b8a54f855
Keristero Kerivinix <Keristero@gmail.com>
2021-12-08 17:11:07 +1300

    set bot colors

--- onb-server/scripts/internet_cyberworld/main.lua
+++ onb-server/scripts/internet_cyberworld/main.lua
@@ -36,3 +36,14 @@ ezevents.add_listener('new_area_added',function (area_id)
     --add npcs to areas added while server is running
     add_npcs_to_area(area_id)
 end)
+
+-- color bots on transfer
+local bot_color = { r = 50, g = 248, b = 109, a = 255 }
+
+function handle_player_transfer(player_id)
+  local area_id = Net.get_player_area(player_id)
+
+  for _, bot_id in ipairs(Net.list_bots(area_id)) do
+    Net.set_bot_minimap_color(bot_id, bot_color)
+  end
+end
