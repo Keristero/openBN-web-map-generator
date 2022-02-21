2881c4a005a5cc3dc21cfbe947578bcc1c840c0a
Keristero Kerivinix <Keristero@gmail.com>
2022-02-21 14:37:57 +1300

    remove / update default assets

--- onb-server/assets/shared/objects/placeholder_npc.tsx
+++ onb-server/assets/shared/objects/placeholder_npc.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="placeholder_npc" tilewidth="15" tileheight="33" tilecount="1" columns="1" objectalignment="top">
+<tileset version="1.5" tiledversion="1.7.2" name="placeholder_npc" tilewidth="15" tileheight="33" tilecount="1" columns="1" objectalignment="bottom">
  <tileoffset x="0" y="3"/>
  <image source="placeholder_npc.png" width="15" height="33"/>
 </tileset>
