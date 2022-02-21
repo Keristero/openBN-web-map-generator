772332d39f5998cd83618b2c0087d832ee5cc0e6
Keristero Kerivinix <Keristero@gmail.com>
2022-02-21 14:45:13 +1300

    fail safe default image for images

--- map-exporter/generate-image-board.js
+++ map-exporter/generate-image-board.js
@@ -5,10 +5,16 @@ let { writeFile } = require('fs/promises')
 const fs = require('fs')
 const path = require('path')
 
+const default_image_path = path.resolve('.','map-exporter','default_image.png')
+
 async function generate_image_board_image(image_url) {
-    let input_image_path = `temp_board_image.png`
-    await downloadFile(image_url, input_image_path)
-    let input_image = await loadImage(input_image_path)
+    //let input_image_path = `temp_board_image.png`
+    //await downloadFile(image_url, input_image_path)
+    try{
+        var input_image = await loadImage(image_url)
+    }catch(e){
+        var input_image = await loadImage(default_image_path)
+    }
 
     let canvas_board = createCanvas(64, 64)
     let ctx_board = canvas_board.getContext('2d')
@@ -39,11 +45,11 @@ async function generate_image_board_image(image_url) {
     return canvas_board
 }
 
-async function generateTSX(tsx_path, tileHash) {
+async function generateTSX(tsx_path, image_name) {
     let doc = `<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="${tileHash}" tilewidth="64" tileheight="64" tilecount="1" columns="1" objectalignment="top">
+<tileset version="1.5" tiledversion="1.5.0" name="${image_name}" tilewidth="64" tileheight="64" tilecount="1" columns="1" objectalignment="top">
     <tileoffset x="0" y="0"/>
-    <image source="./${tileHash}.png" width="64" height="64"/>
+    <image source="./${image_name}.png" width="64" height="64"/>
     <tile id="0">
         <objectgroup draworder="index" id="2">
             <object id="2" x="-3.25193" y="41.1807" width="35.7708" height="6.88694" rotation="334.505"/>
@@ -56,21 +62,17 @@ async function generateTSX(tsx_path, tileHash) {
 
 function generate_image_board(image_url){
     return new Promise(async(resolve,reject)=>{
-        try{
-            let path_generated_assets = path.join('.', 'onb-server','assets', 'generated')
-            let file_name = fastHash(image_url)
-            let relative_tsx_path = `../assets/generated/${file_name}.tsx`
-            let canvas = await generate_image_board_image(image_url)
-            await generateTSX(path.join(path_generated_assets,`${file_name}.tsx`),file_name)
-            let out = fs.createWriteStream(path.join(path_generated_assets,`${file_name}.png`))
-            let stream = canvas.createPNGStream(`${file_name}.png`)
-            stream.pipe(out)
-            out.on('finish', () =>{
-                resolve(relative_tsx_path)
-            })
-        }catch(e){
-            reject(e)
-        }
+        let path_generated_assets = path.join('.', 'onb-server','assets', 'generated')
+        let file_name = fastHash(image_url)
+        let relative_tsx_path = `../assets/generated/${file_name}.tsx`
+        let canvas = await generate_image_board_image(image_url)
+        await generateTSX(path.join(path_generated_assets,`${file_name}.tsx`),file_name)
+        let out = fs.createWriteStream(path.join(path_generated_assets,`${file_name}.png`))
+        let stream = canvas.createPNGStream(`${file_name}.png`)
+        stream.pipe(out)
+        out.on('finish', () =>{
+            resolve(relative_tsx_path)
+        })
     })
 }
 
